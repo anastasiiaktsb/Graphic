2f5476fa384f047b76669a571d73a5a8a91a5c80
anastasiiaktsb <45684119+anastasiiaktsb@users.noreply.github.com>
2020-10-15 20:40:21 +0300

    Add files via upload

---
+++ Graphic_1/Dot.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Drawing;
+
+namespace Graphic1
+{
+    class Dot
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Z { get; set; } = 1;
+        public Dot() { }
+        public Dot(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+        public void Draw(Graphics graphics, SolidBrush solidBrush)
+        {
+            graphics.FillRectangle(solidBrush, X, Y, 5, 5);
+        }
+        public void Rotate(double angle, Dot center)
+        {
+            float xCopy = X;
+            float yCopy = Y;
+            float[] result = new float[3];
+            double[,] turnMatrix = new double[3, 3];
+            turnMatrix[0, 0] = Math.Cos(angle * Math.PI / 180.0);
+            turnMatrix[0, 1] = Math.Sin(angle * Math.PI / 180.0);
+            turnMatrix[0, 2] = 0;
+            turnMatrix[1, 0] = -Math.Sin(angle * Math.PI / 180.0);
+            turnMatrix[1, 1] = Math.Cos(angle * Math.PI / 180.0);
+            turnMatrix[1, 2] = 0;
+            turnMatrix[2, 0] = -center.X * (Math.Cos(angle * Math.PI / 180.0) - 1) + center.Y * Math.Sin(angle * Math.PI / 180.0);
+            turnMatrix[2, 1] = -center.X * Math.Sin(angle * Math.PI / 180.0) - center.Y * (Math.Cos(angle * Math.PI / 180.0) - 1);
+            turnMatrix[2, 2] = 1;
+            X = (float)(xCopy * turnMatrix[0,0]+ yCopy * turnMatrix[1,0]+Z*turnMatrix[2,0]);
+            Y = (float)(xCopy * turnMatrix[0, 1] + yCopy * turnMatrix[1, 1] + Z * turnMatrix[2, 1]);
+            Z = 1;
+        }
+        public void MedianIntersection(Dot dot1, Dot dot2, Dot dot3)
+        {
+            int x, y;
+            double side1 = Math.Sqrt(Math.Pow((dot2.X - dot3.X), 2) + Math.Pow((dot2.Y - dot3.Y), 2));
+            double side2 = Math.Sqrt(Math.Pow((dot1.X - dot3.X), 2) + Math.Pow((dot1.Y - dot3.Y), 2));
+            double side3 = Math.Sqrt(Math.Pow((dot1.X - dot2.X), 2) + Math.Pow((dot1.Y - dot2.Y), 2));
+            x = (int)((side1 * dot1.X + side2 * dot2.X + side3 * dot3.X) / (side1 + side2 + side3));
+            y = (int)((side1 * dot1.Y + side2 * dot2.Y + side3 * dot3.Y) / (side1 + side2 + side3));
+            X = x;
+            Y = y;
+            Z = 1;
+        }
+    }
+}
---
+++ Graphic_1/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Graphic1
+{
+    public partial class Form1 : Form
+    {
+        Dot linDot1;
+        Dot linDot2;
+        Dot linCenter;
+        static List<Dot> cubeDots;
+        static Dot center, dot, dot1, dot2, dot3, dot5, dot6, dot7, tet1, tet2, tet3, incenter;
+        static List<Dot> tetDots;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Graphics graphics = CreateGraphics();
+            graphics.Clear(Color.White);
+            Pen pen = new Pen(Color.DarkBlue, 3);
+            SolidBrush solidBrush = new SolidBrush(Color.DarkBlue);
+            SolidBrush solidBrush1 = new SolidBrush(Color.DarkOrange);
+            linDot1 = new Dot(Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text));
+            linDot2 = new Dot((int)(linDot1.X + Convert.ToInt32(textBox3.Text)), (int)(linDot1.Y + Convert.ToInt32(textBox3.Text)));
+            linDot2.Rotate(Convert.ToInt32(textBox4.Text), linDot1);
+            graphics.DrawLine(pen, linDot1.X, linDot1.Y, linDot2.X, linDot2.Y);
+            linCenter = new Dot((int)(linDot1.X / 2 + linDot2.X / 2), (int)(linDot1.Y / 2 + linDot2.Y / 2));
+            center = new Dot((int)(linCenter.X-300), (int)(linCenter.Y - 150));
+            dot = new Dot((int)(linCenter.X - 300), (int)(linCenter.Y - 200));
+            dot1 = new Dot((int)(linCenter.X - 300), (int)(linCenter.Y - 200));
+            dot2 = new Dot((int)(linCenter.X - 300), (int)(linCenter.Y - 200));
+            dot1.Rotate(90, center);
+            dot2.Rotate(-135, center);
+            dot3 = new Dot((int)dot1.X, (int)dot1.Y - 50);
+            dot3.Rotate(-135, dot1);
+            dot5 = new Dot((int)dot1.X, (int)dot1.Y - 50);
+            dot6 = new Dot((int)dot2.X, (int)dot2.Y - 50);
+            dot7 = new Dot((int)dot3.X, (int)dot3.Y - 50);
+            graphics.DrawLine(pen, center.X, center.Y, dot.X, dot.Y);
+            graphics.DrawLine(pen, center.X, center.Y, dot1.X, dot1.Y);
+            graphics.DrawLine(pen, center.X, center.Y, dot2.X, dot2.Y);
+            graphics.DrawLine(pen, dot1.X, dot1.Y, dot3.X, dot3.Y);
+            graphics.DrawLine(pen, dot2.X, dot2.Y, dot3.X, dot3.Y);
+            graphics.DrawLine(pen, dot.X, dot.Y, dot5.X, dot5.Y);
+            graphics.DrawLine(pen, dot5.X, dot5.Y, dot7.X, dot7.Y);
+            graphics.DrawLine(pen, dot6.X, dot6.Y, dot7.X, dot7.Y);
+            graphics.DrawLine(pen, dot6.X, dot6.Y, dot.X, dot.Y);
+            graphics.DrawLine(pen, dot5.X, dot5.Y, dot1.X, dot1.Y);
+            graphics.DrawLine(pen, dot2.X, dot2.Y, dot6.X, dot6.Y);
+            graphics.DrawLine(pen, dot7.X, dot7.Y, dot3.X, dot3.Y);
+            cubeDots = new List<Dot> { center, dot, dot1, dot2, dot3, dot5, dot6, dot7 };
+            tet1 = new Dot((int)(linCenter.X + 200), (int)(linCenter.Y + 100));
+            tet3 = new Dot((int)tet1.X+50, (int)tet1.Y);
+            tet3.Rotate(30,tet1);
+            tet2 = new Dot((int)tet1.X + 100, (int)tet1.Y);
+            graphics.DrawLine(pen, tet1.X, tet1.Y, tet2.X, tet2.Y);
+            graphics.DrawLine(pen, tet2.X, tet2.Y, tet3.X, tet3.Y);
+            graphics.DrawLine(pen, tet3.X, tet3.Y, tet1.X, tet1.Y);
+            incenter = new Dot();
+            incenter.MedianIntersection(tet1,tet2,tet3);
+            incenter.Y -= 75;
+            graphics.DrawLine(pen, tet1.X, tet1.Y, incenter.X, incenter.Y);
+            graphics.DrawLine(pen, tet2.X, tet2.Y, incenter.X, incenter.Y);
+            graphics.DrawLine(pen, tet3.X, tet3.Y, incenter.X, incenter.Y);
+            tetDots = new List<Dot> { tet1, tet2, tet3, incenter};
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Timer timer3 = new Timer();
+            timer3.Tick += new EventHandler(RotateFigures);
+            timer3.Interval = 250;
+            timer3.Start();
+
+        }
+        public void RotateFigures(object sender, EventArgs e)
+        {
+            
+            foreach (Dot dt in cubeDots)
+            {
+                dt.Rotate(10, linCenter);
+            }
+            Graphics graphics = CreateGraphics();
+            graphics.Clear(Color.White);
+            Pen pen = new Pen(Color.DarkBlue, 3);
+            graphics.DrawLine(pen, linDot1.X, linDot1.Y, linDot2.X, linDot2.Y);
+            graphics.DrawLine(pen, center.X, center.Y, dot.X, dot.Y);
+            graphics.DrawLine(pen, center.X, center.Y, dot1.X, dot1.Y);
+            graphics.DrawLine(pen, center.X, center.Y, dot2.X, dot2.Y);
+            graphics.DrawLine(pen, dot1.X, dot1.Y, dot3.X, dot3.Y);
+            graphics.DrawLine(pen, dot2.X, dot2.Y, dot3.X, dot3.Y);
+            graphics.DrawLine(pen, dot.X, dot.Y, dot5.X, dot5.Y);
+            graphics.DrawLine(pen, dot5.X, dot5.Y, dot7.X, dot7.Y);
+            graphics.DrawLine(pen, dot6.X, dot6.Y, dot7.X, dot7.Y);
+            graphics.DrawLine(pen, dot6.X, dot6.Y, dot.X, dot.Y);
+            graphics.DrawLine(pen, dot5.X, dot5.Y, dot1.X, dot1.Y);
+            graphics.DrawLine(pen, dot2.X, dot2.Y, dot6.X, dot6.Y);
+            graphics.DrawLine(pen, dot7.X, dot7.Y, dot3.X, dot3.Y);
+            foreach (Dot dt in tetDots)
+            {
+                dt.Rotate(-10, linCenter);
+            }
+            graphics.DrawLine(pen, tet1.X, tet1.Y, tet2.X, tet2.Y);
+            graphics.DrawLine(pen, tet2.X, tet2.Y, tet3.X, tet3.Y);
+            graphics.DrawLine(pen, tet3.X, tet3.Y, tet1.X, tet1.Y);
+            graphics.DrawLine(pen, tet1.X, tet1.Y, incenter.X, incenter.Y);
+            graphics.DrawLine(pen, tet2.X, tet2.Y, incenter.X, incenter.Y);
+            graphics.DrawLine(pen, tet3.X, tet3.Y, incenter.X, incenter.Y);
+        }
+        
+    }
+    
+}
